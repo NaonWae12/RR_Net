c9df725c3797a24b90d180a4bec43523afb433e9
saefulanwar7721 <saefulanwar7721@gmail.com>
2026-03-28 23:18:32 +0700

    fix(mikrotik): restore nas-identifier to backend code template for V6 backward compatibility

--- BE/internal/service/network_service.go
+++ BE/internal/service/network_service.go
@@ -1886,10 +1886,10 @@ func (s *NetworkService) generateMikrotikVPNScript(router *network.Router) strin
 /system identity set name="%s"
 
 ## RADIUS & HOTSPOT SETUP
-/radius add address=%s secret=%s service=hotspot,ppp comment="RR-NET"
+/radius add address=%s secret=%s service=hotspot,ppp comment="RR-NET" nas-identifier="%s"
 /ip hotspot profile set [ find default=yes ] use-radius=yes
 /ppp aaa set use-radius=yes
-`, router.Name, vpnInterfaceCmd, vpnSubnet, vpnSubnet, router.Name, gatewayIP, radiusSec)
+`, router.Name, vpnInterfaceCmd, vpnSubnet, vpnSubnet, router.Name, gatewayIP, radiusSec, nasID)
 
 	return script
 }
